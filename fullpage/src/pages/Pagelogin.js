58b99243ffaea0c989dfd9b4870bd2e96767aeba
SamaGamall <samagamal024@gmail.com>
2023-05-17 03:12:09 +0300

    essage

--- fullpage/src/pages/Pagelogin.js
+++ fullpage/src/pages/Pagelogin.js
@@ -3,14 +3,14 @@ import { Link, NavLink  } from "react-router-dom";
 import Login from'../comp/Login';
 import Navbarinside from '../comp/Navbarinside';
 
-
+import ViewAllUsersPage from '../comp/ViewAllUsersPage';
 
 
 const Pagelogin = () => {
   return (
     <>
     <Navbarinside />
-    <Login />
+    <ViewAllUsersPage />
     </>
   );
 }
